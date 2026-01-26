9f6553c1fdacb53cdee807100efbabefbf5875d6
yooyoungjae0627 <yooyoungjae0627@naver.com>
2026-01-26 18:00:28 +0900

    [개발자:유영재] GamePlayTags 등록

--- UE5_Multi_Shooter/Source/UE5_Multi_Shooter/GAS/MosesGameplayTags.cpp
+++ UE5_Multi_Shooter/Source/UE5_Multi_Shooter/GAS/MosesGameplayTags.cpp
@@ -1,16 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "MosesGameplayTags.h"
 #include "GameplayTagsManager.h"
+
+// (선택) 로그 넣고 싶으면 여기에 MosesLogChannels include
+// #include "UE5_Multi_Shooter/MosesLogChannels.h"
 
 /** 싱글톤 인스턴스 정의 */
 FMosesGameplayTags FMosesGameplayTags::GameplayTags;
 bool FMosesGameplayTags::bInitialized = false;
 
-/**
- * 엔진 부팅 시 호출되어
- * 모든 네이티브 GameplayTag를 등록
- */
 void FMosesGameplayTags::InitializeNativeTags()
 {
 	// Native GameplayTags는 "엔진 초기화 초반"에만 추가 가능.
@@ -24,13 +21,12 @@ void FMosesGameplayTags::InitializeNativeTags()
 	bInitialized = true;
 
 	UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
-	GameplayTags.AddAllTags(Manager);
+	AddAllTags(Manager);
+
+	// (선택) 증거 로그
+	// UE_LOG(LogMosesAsset, Log, TEXT("[TAGS] Native GameplayTags initialized"));
 }
 
-/**
- * 단일 네이티브 GameplayTag 등록
- * - 문자열 기반 태그를 엔진에 정식 등록
- */
 void FMosesGameplayTags::AddTag(UGameplayTagsManager& Manager, FGameplayTag& OutTag, const ANSICHAR* TagName, const ANSICHAR* TagComment)
 {
 	OutTag = Manager.AddNativeGameplayTag(
@@ -39,25 +35,34 @@ void FMosesGameplayTags::AddTag(UGameplayTagsManager& Manager, FGameplayTag& Out
 	);
 }
 
-/**
- * 프로젝트에서 사용할 모든 GameplayTag 정의
- */
 void FMosesGameplayTags::AddAllTags(UGameplayTagsManager& Manager)
 {
 	// 초기화 상태 태그
-	AddTag(Manager, InitState_Spawned, "InitState.Spawned", "Actor just spawned");
-	AddTag(Manager, InitState_DataAvailable, "InitState.DataAvailable", "Required data is ready");
-	AddTag(Manager, InitState_DataInitialized, "InitState.DataInitialized", "Internal initialization done");
-	AddTag(Manager, InitState_GameplayReady, "InitState.GameplayReady", "Ready for gameplay");
+	AddTag(Manager, GameplayTags.InitState_Spawned, "InitState.Spawned", "Actor just spawned");
+	AddTag(Manager, GameplayTags.InitState_DataAvailable, "InitState.DataAvailable", "Required data is ready");
+	AddTag(Manager, GameplayTags.InitState_DataInitialized, "InitState.DataInitialized", "Internal initialization done");
+	AddTag(Manager, GameplayTags.InitState_GameplayReady, "InitState.GameplayReady", "Ready for gameplay");
 
 	// 입력 의미 태그
-	AddTag(Manager, InputTag_Move, "InputTag.Move", "Move input");
-	AddTag(Manager, InputTag_Look_Mouse, "InputTag.Look.Mouse", "Mouse look input");
+	AddTag(Manager, GameplayTags.InputTag_Move, "InputTag.Move", "Move input");
+	AddTag(Manager, GameplayTags.InputTag_Look_Mouse, "InputTag.Look.Mouse", "Mouse look input");
 
-	// --- Combat / Dead 정책 태그
-	AddTag(Manager, State_Phase_Combat, "State.Phase.Combat", "In combat phase (server authoritative)");
-	AddTag(Manager, State_Dead, "State.Dead", "Dead state (server authoritative)");
+	// Combat / Dead 정책 태그
+	AddTag(Manager, GameplayTags.State_Phase_Combat, "State.Phase.Combat", "In combat phase (server authoritative)");
+	AddTag(Manager, GameplayTags.State_Dead, "State.Dead", "Dead state (server authoritative)");
+
+	// ------------------------------
+	// Weapon Tags
+	// ------------------------------
+	AddTag(Manager, GameplayTags.Weapon_Rifle_A, "Weapon.Rifle.A", "Rifle A (DataAsset key)");    
+	AddTag(Manager, GameplayTags.Weapon_Rifle_B, "Weapon.Rifle.B", "Rifle B (DataAsset key)");    
+	AddTag(Manager, GameplayTags.Weapon_Rifle_C, "Weapon.Rifle.C", "Rifle C (DataAsset key)");    
+	AddTag(Manager, GameplayTags.Weapon_Pistol, "Weapon.Pistol", "Pistol (DataAsset key)");      
+	AddTag(Manager, GameplayTags.Weapon_Grenade, "Weapon.Grenade", "Grenade (DataAsset key)");    
+
+	// ------------------------------
+	// Zombie Tags (optional)
+	// ------------------------------
+	AddTag(Manager, GameplayTags.Zombie_Attack_A, "Zombie.Attack.A", "Zombie attack type A");      
+	AddTag(Manager, GameplayTags.Zombie_Attack_B, "Zombie.Attack.B", "Zombie attack type B");      
 }
-
-
-
--- UE5_Multi_Shooter/Source/UE5_Multi_Shooter/GAS/MosesGameplayTags.h
+++ UE5_Multi_Shooter/Source/UE5_Multi_Shooter/GAS/MosesGameplayTags.h
@@ -7,21 +7,28 @@ class UGameplayTagsManager;
 /**
  * Native GameplayTags Singleton
  * - 프로젝트 전역에서 사용할 네이티브 GameplayTag를 "엔진 초기화 초반"에 1회 등록한다.
+ *
+ * Lyra 스타일:
+ * - "태그 문자열"은 코드에 상수로 고정(SSOT)
+ * - 어디서든 FMosesGameplayTags::Get().Weapon_Rifle_A 형태로 사용
  */
 struct FMosesGameplayTags
 {
+public:
 	/** 싱글톤 접근자 */
 	static const FMosesGameplayTags& Get() { return GameplayTags; }
 
 	/** 엔진 부팅 초기에 1회 호출되어 네이티브 태그 등록 */
 	static void InitializeNativeTags();
 
+private:
 	/** 단일 태그 등록 헬퍼 (Manager 기반으로만 등록한다) */
-	void AddTag(UGameplayTagsManager& Manager, FGameplayTag& OutTag, const ANSICHAR* TagName, const ANSICHAR* TagComment);
+	static void AddTag(UGameplayTagsManager& Manager, FGameplayTag& OutTag, const ANSICHAR* TagName, const ANSICHAR* TagComment);
 
-	/** 단일 태그 등록 헬퍼 (Manager 기반으로만 등록한다) */
-	void AddAllTags(UGameplayTagsManager& Manager);
+	/** 프로젝트에서 사용할 모든 GameplayTag 정의 */
+	static void AddAllTags(UGameplayTagsManager& Manager);
 
+public:
 	/* ---------- Init State Tags ---------- */
 	FGameplayTag InitState_Spawned;
 	FGameplayTag InitState_DataAvailable;
@@ -35,6 +42,17 @@ struct FMosesGameplayTags
 	/* ---------- Combat State Tags ---------- */
 	FGameplayTag State_Phase_Combat;
 	FGameplayTag State_Dead;
+
+	/* ---------- Weapon Tags ---------- */
+	FGameplayTag Weapon_Rifle_A;     
+	FGameplayTag Weapon_Rifle_B;     
+	FGameplayTag Weapon_Rifle_C;     
+	FGameplayTag Weapon_Pistol;      
+	FGameplayTag Weapon_Grenade;     
+
+	/* ---------- Zombie Tags ---------- */
+	FGameplayTag Zombie_Attack_A;    
+	FGameplayTag Zombie_Attack_B;    
 
 private:
 	/** 전역 싱글톤 인스턴스 */
